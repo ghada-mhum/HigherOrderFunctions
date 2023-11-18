c27d47f19fca18c66b06e6dbc41333e88390aa21
ghada-mhum <ghada.mhum@gmail.com>
2023-11-18 22:45:45 +0300

    first commit

---
+++ HigherOrderFunction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HigherOrderFunction
+//
+//  Created by ghada Mohammad on 04/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HigherOrderFunction/HigherOrderFunctionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HigherOrderFunctionApp.swift
+//  HigherOrderFunction
+//
+//  Created by ghada Mohammad on 04/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct HigherOrderFunctionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
